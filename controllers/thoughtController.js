db934fcd6467d2c3e12cb1e7b40df1ac66b3e550
neilmkflyingk <126199320+neilmkflyingk@users.noreply.github.com>
2023-09-09 11:29:43 -0700

    get one thought control

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -16,16 +16,24 @@ module.exports = {
         res.sendStatus(400);
       });
   },
-  // Get a thought
-  getSingleThoughts(req, res) {
-    Thought.findOne({ _id: req.params.thoughtId })
-      .select('-__v')
-      .then((thought) =>
-        !thought
-          ? res.status(404).json({ message: 'No thought with that ID' })
-          : res.json(thought)
-      )
-      .catch((err) => res.status(500).json(err));
+  // Get one thought
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .populate({
+        path: "reactions",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: "No thought with this id!" });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
   },
   // Create a thought
   createThought(req, res) {
