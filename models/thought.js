2e427f2e7b97b9a1007ea3cb91a79d02ccae664d
neilmkflyingk <126199320+neilmkflyingk@users.noreply.github.com>
2023-09-09 11:59:18 -0700

    reaction schema

--- models/thought.js
+++ models/thought.js
@@ -1,5 +1,37 @@
-const { Schema, model } = require('mongoose');
-const reactionSchema = require("./reaction");
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 // Schema to create a thought model
 const thoughtSchema = new Schema(
