b493e24f631b61d94dba4856be9573ee8e8a2126
neilmkflyingk <126199320+neilmkflyingk@users.noreply.github.com>
2023-09-08 19:59:46 -0700

    reconfig files

--- kayvon_walkthrough/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./usereRoutes');
-const thoughtRoutes = require('./userRoutes');
-
-router.use('/users', userRoutes);
-router.use('/thoughts', thoughtRoutes);
-
-module.exports = router;
